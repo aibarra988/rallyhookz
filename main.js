d213f1345186e7c5b5bc2ca3945741b9805b5850
Hung Nguyen <hung.nguyen@ca.com>
2017-05-12 15:02:56 -0500

    make this thing work in heroku

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 1337));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! Now go away.')
 })
 
-app.listen(1337, function () {
-  console.log('Listening on port 1337...')
+app.listen(app.get('port'), function () {
+  console.log('Listening on port', app.get('port'), '...')
 })
-
