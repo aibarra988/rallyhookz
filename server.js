6e6079ce82aea298b0e80e1efb5227c623d70ad1
Abraham Ibarra <aibarra988@gmail.com>
2017-05-14 22:05:35 -0500

    Fixing listening port, changing Profile target, and removing unused js files.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 let app = require('./app');
 
-app.set('port', 3001);
+app.set('port', 1337);
 
 let server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
