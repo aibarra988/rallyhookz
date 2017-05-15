336be8f0e2be0a00446699951bd1fd7a41c865a2
Abraham Ibarra <aibarra988@gmail.com>
2017-05-15 10:02:46 -0500

    Adding regex and a sense of acting on a merged pr

--- routes.js
+++ routes.js
@@ -18,17 +18,27 @@ router.get('/', (req, res) => {
 router.post('/listen', (req, res) => {
 	// Process git event and find the Rally Defect Id in title of pull request
 	const prTitle = req.body.pull_request.title;
-	const rallyDefectId = 'DE1';	// regex to get rally defect number from pr title
+	const idFilter = /DE[0-9]+/;	// regex to get rally defect number from pr title
+	const reResult = idFilter.exec(prTitle);
+	const rallyDefectId = reResult[0];
 
-	// Find and update Rally ticket
-	getRallyDefectById(rallyDefectId)
-		.then(updateInRally)
-		.then(response => console.log("I am in updateInRally ", response))
-		.catch(error => console.log(error))
-		.then((err, success) => {
-			if (err) res.send(err);
-			res.send('Successfully updated ' + rallyDefectId + ' in Rally');
-		}); 
+	console.log('idFilter: ', idFilter);
+	console.log('reResult: ', reResult);
+
+	if (req.body.action === "merged") {
+		// Find and update Rally ticket
+		getRallyDefectById(rallyDefectId)
+			.then(updateInRally)
+			.then(response => console.log("I am in updateInRally ", response))
+			.catch(error => console.log(error))
+			.then((err, success) => {
+				if (err) res.send(err);
+				res.send('Successfully updated ' + rallyDefectId + ' in Rally');
+			}); 	
+	} else {
+		res.send("PR action was not 'merged'");
+	}
+
 });
 
 function getRallyDefectById(rallyDefectId) {
